e7c93b4274a1b7f9f87fd6dd7a70007a2d9a595d
EzequielOmar <ezequiel.omar.vazquez@gmail.com>
2022-02-14 13:01:57 -0300

    agregando proyecto y leves cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,15 +12,6 @@ export const environment = {
     messagingSenderId: '396906618797',
     measurementId: 'G-98BZ51WYDZ',
   },
-  firebaseConfig: {
-    apiKey: 'AIzaSyBVcSCRBEwmTIc7tK0ajyS5HJlEFGBHUz0',
-    authDomain: 'ezequielomar-2390d.firebaseapp.com',
-    projectId: 'ezequielomar-2390d',
-    storageBucket: 'ezequielomar-2390d.appspot.com',
-    messagingSenderId: '396906618797',
-    appId: '1:396906618797:web:6b6005cd8322f8de214b24',
-    measurementId: 'G-98BZ51WYDZ',
-  },
   production: false,
 };
 
